d4441e53deb3e7e28b6e8d811cc6e042819e5b57
MJ-Curtido <manu17pc@gmail.com>
2023-01-16 13:04:35 +0100

    Agregar archivos de proyecto.

---
+++ DEINT-CalculoIMC/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DEINT_CalculoIMC;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DEINT-CalculoIMC/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DEINT_CalculoIMC;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
